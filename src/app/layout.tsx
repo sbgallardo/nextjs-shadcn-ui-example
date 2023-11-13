d19f8309592d09095abe282b5373c2cdd864a4f0
Sebastián G <seba.gallardog@duocuc.cl>
2023-11-12 22:31:34 -0300

    Theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
+import { ThemeProvider } from "@/components/theme-provider"
 import './globals.css'
 
 const geist = GeistSans
@@ -17,7 +18,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={geist.className}>
-        {children}
+          <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+          >
+              {children}
+          </ThemeProvider>
       </body>
     </html>
   )
